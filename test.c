9a899fd7339f2bbe39cb1524dcf17d07ae5eba5a
JokerXJX <96376985+JokerXJX@users.noreply.github.com>
2022-01-06 19:51:51 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,93 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//测试三子棋游戏
+#include<stdio.h>
 
+//int main()
+//{
+//	printf("Practice makes perfect!");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	printf("v v\n");
+//	printf(" v ");
+//	return 0;
+//}
 
-#include"game.h"
-void menu()
-{
-	printf("*********************\n");
-	printf("****1.play 0.exit****\n");
-	printf("*********************\n");
-}
+//int main()
+//{
+//	printf("The size of short is:%d bytes\n",sizeof(short));
+//	printf("The size of int is:%d bytes\n", sizeof(int));
+//	printf("The size of long is:%d bytes\n", sizeof(long));
+//	printf("The size of long long is:%d bytes\n", sizeof(long long));
+//	return 0;
+//}
 
-//   |   |
-//---|---|---  //分割行
-//   |   |
-//---|---|---  //分割行
-//   |   |   
-//游戏的实现
-void game()
-{
-	char ret = 0;
-	//数组存放走出棋盘信息
-	char board[ROW][COL]={0};//全空格
-	//初始化棋盘-数组
-	InitBoard(board,ROW,COL);
-	//打印棋盘
-	DisplayBoard(board,ROW,COL);
-	//下棋
-	while (1)
-	{
-		//玩家下棋
-		PlayerMove(board,ROW,COL);
-		DisplayBoard(board, ROW, COL);
-		//判断玩家输赢
-		ret=IsWin(board, ROW, COL);
-		if (ret != 'C')
-		{
-			break;
-		}
-		//电脑下棋
-		ComputerMove(board, ROW, COL);
-		DisplayBoard(board, ROW, COL);
-		//判断电脑输赢
-		ret=IsWin(board, ROW, COL);
-		if (ret != 'C')
-		{
-			break;
-		}
-	}
-	if (ret == '*')
-	{
-		printf("玩家赢");
-	}
-	else if (ret == '#')
-	{
-		printf("电脑赢");
-	}
-	else
-	{
-		printf("平局");
-	}
-}  
+//int main()
+//{
+//	int x = 1234;
+//	printf("x=0%o\n", x);
+//	printf("x=0x%x", x);
+//	return 0;
+//}
 
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	while (n)
+//	{
+//		printf("%d", n % 10);
+//		n /= 10;
+//	}
+//	return 0;
+//}
 
-void test()
-{
-	int input = 0;
-	srand((unsigned int)time(NULL));
-	do
-	{
-		menu();
-		printf("请选择:>");
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("退出游戏\n");
-			break;
-		default:
-			printf("选择错误,请重新选择\n");
-			break;
-		}	
-	} while (input);
-}
-
-int main()
-{
-	test();
-	return 0;
-}
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar())!=EOF)
+//	{
+//		printf("%c\n", (ch + 32));
+//		getchar();
+//	}
+//	return 0;
+//}

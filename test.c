0442ef5c925f1eb9bd8e659f8e1ff61d6f08726c
JokerXJX <96376985+JokerXJX@users.noreply.github.com>
2022-01-06 21:43:52 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,55 +1,121 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+﻿#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
 //int main()
 //{
-//	printf("Practice makes perfect!");
-//	return 0;
-//}
-//
-//int main()
-//{
-//	printf("v v\n");
-//	printf(" v ");
-//	return 0;
-//}
-
-//int main()
-//{
-//	printf("The size of short is:%d bytes\n",sizeof(short));
-//	printf("The size of int is:%d bytes\n", sizeof(int));
-//	printf("The size of long is:%d bytes\n", sizeof(long));
-//	printf("The size of long long is:%d bytes\n", sizeof(long long));
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *    \n");
+//	printf("    *  *    \n");
 //	return 0;
 //}
 
 //int main()
 //{
-//	int x = 1234;
-//	printf("x=0%o\n", x);
-//	printf("x=0x%x", x);
+//	long int n = 0xABCDEF;
+//	printf("%15d", n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ret = printf("Hello world");
+//	printf("\n");
+//	printf("%d\n", ret);
 //	return 0;
 //}
 
 //int main()
 //{
 //	int n = 0;
-//	scanf("%d", &n);
-//	while (n)
+//	float a = 0.0, b = 0.0, c = 0.0;
+//	scanf("%d %f %f %f", &n, &a, &b, &c);
+//	printf("The each subject score of  NO. %d is %.2f %.2f %.2f.\n", n, a, b, c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char ch = 0;
+//	scanf("%c", &ch);
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
 //	{
-//		printf("%d", n % 10);
-//		n /= 10;
+//		int j = 0;
+//		for (j = 0; j < 5 - 1 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%c ", ch);
+//		}
+//		printf("\n");
 //	}
 //	return 0;
 //}
 
 //int main()
 //{
-//	int ch = 0;
-//	while ((ch = getchar())!=EOF)
+//	char arr[] = { 73,32,99,97,110,32,100,111,32,105,116,33 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
 //	{
-//		printf("%c\n", (ch + 32));
-//		getchar();
+//		printf("%c ", arr[i]);
 //	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	int date = 0;
+//	scanf("%4d%2d%2d",&year,&month,&date);
+//	printf("year=%d\n", year);
+//	printf("month=%02d\n", month);// %02d表示以2列的方式输出一个整数（不足2列补0 ）
+//	printf("date=%02d\n", date);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		printf("%d\n", 1<<n);
+//	}
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//	char n = 0;
+//	scanf("%c", &n);
+//	printf("%d\n", n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int c = 0;
+//	int res = 0;
+//	scanf("%d%d", &a, &c);
+//	printf("ret=%d\n", (-8 + 22) * a - 10 + c / 2);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	printf("%d %d", a / b, a % b);
 //	return 0;
 //}
